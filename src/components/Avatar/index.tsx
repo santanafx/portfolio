ca06a26404e5ddbbe19b505bf616a150facecf52
Lucas Santana <santanafx@hotmail.com>
2023-08-10 11:25:48 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/santanafx.png" alt="" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/santanafx.png" alt="" />
-    <Titulo fontSize={20}>Lucas Santana</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas Santana</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        santanafx
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
